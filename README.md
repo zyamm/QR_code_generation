5e62b520a2a7a2085f5029d9be04be3bd8df691e
zyamm <70940077+zyamm@users.noreply.github.com>
2020-12-09 14:18:59 +0900

    Develop (#1)
    * Describe the QR code generation process with the input value fixed
    
    * Added the ability to receive command line arguments and URL
    
    * Added URL input value check
    
    * Create README.md
    
    * add gitignore
    
    * chenge gitignore

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # QR_code_generation
-Command line application to generate QR code
+
+This is the command line application to generate QR code.
+
+
+When you enter the URL, a QR code image with any file name will be generated in the executed directory.
+Output uses SVG format.
+Using a third party package 'qrcode'.
+
+
+Assumed operating environment:
+  Works with Python 3.6 and above
+  OS does not matter
+  Modern browser that can display SVG
+
+
+When running on the command line, type:　
+$ python make_qr.py (command line argument)
+
+Enter the name of the output image file in ().
+Example:
+$ python make_qr.py python.svg
+
---
+++ make_qr.py
@@ -0,0 +1,30 @@
+import qrcode  # Import the package for QR code generation
+import qrcode.image.svg  # Import module to save QR code as SVG image
+
+
+class MakeQR:
+    def __init__(self, output):
+        self.url = ''  # The initial value of the URL is empty
+        self.output = output
+
+    def run(self):
+        url = input('Enter URL : ')
+        if not url.startswith('http'):  # Input value check
+            print("The string entered does not start with 'http'")
+            return  # run method ends here, make method is not executed
+        self.url = url
+        self.make()
+
+    def make(self):  # Generate and save the QR code image here
+        factory = qrcode.image.svg.SvgImage  # Declared to make in SVG format
+        img = qrcode.make(self.url, image_factory=factory)
+        img.save(self.output)
+
+
+if __name__ == "__main__":  # True if this module was executed directly
+    import sys
+    args = sys.argv  # Get command line arguments
+    output = args[
+        1]  # From the command-line arguments, put the 1st argument to the variable
+    app = MakeQR(output)
+    app.run()
